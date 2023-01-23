8847b29f98f44b86d85001f4ad8bdfaab2f52451
Rohan Sharma <rohansharma251998@gmail.com>
2023-01-23 16:11:43 +0530

    initial commit

---
+++ src/main/java/com/api/webhook/Controller/WebhookController.java
@@ -0,0 +1,42 @@
+package com.api.webhook.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/webhook")
+public class WebhookController {
+	
+	@Value("${auth.token}")
+	private String authToken;
+	
+	@GetMapping()
+	public ResponseEntity<String> getRequest(@RequestParam String hub_mode,@RequestParam String hub_challenge,@RequestParam String hub_verify_token) {
+		String mode=hub_mode;
+		String challange=hub_challenge;
+		String token=hub_verify_token;
+		
+		System.out.println("hub_mode: "+mode);
+		System.out.println("hub_challenge: "+challange);
+		System.out.println("hub_verify_token: "+token);
+		
+		if(mode != null && token != null){
+	        if(mode.equals("subscribe") && token.equals(authToken)){
+	        	return ResponseEntity.status(HttpStatus.OK).body(challange);
+	        }else{
+	        	return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+	        }
+
+	    }else {
+	    	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+	    }
+	}
+	
+}
